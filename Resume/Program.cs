b5fcabd13feeaa01d35d4eb4f910ab87b83a28ca
RangerLg <nick.manko@yandex.ru>
2021-09-25 23:38:26 +0300

    Init

--- Resume/Program.cs
+++ Resume/Program.cs
@@ -18,9 +18,6 @@ namespace Resume
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
